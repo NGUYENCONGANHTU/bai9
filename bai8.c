c3e8db3d1d85ae27cae216a4a7e9d9e8cea13193
NGUYENCONGANHTU <anhtu20020713@gmail.com>
2023-09-27 20:33:42 +0700

    day

--- bai8.c
+++ bai8.c
@@ -88,6 +88,37 @@ int main ()
 
     return 0;
 }*/
+/*
+int main()
+{
+    int count = 0;
+    float total = 0;
+    char choice;
+    char subject[50];
+    float score;
+
+    do {
+        printf("nhap ten mon hoc: ");
+        scanf("%s", subject);
+        printf("Nhap diem: ");
+        scanf("%f", &score);
+        total += score;
+        count++;
+        printf("Ban co muon tip tuc khong: ");
+        scanf(" %c", &choice);
+
+    } while (choice =='y' || choice =='Y');
+    if(count>0)
+    {
+        double average = total/count;
+        printf("deim trung binh la: %.2lf \n", average);
+    } else
+    {
+        printf("khong co mon hoc nao duoc nhap diem. \n");
+    }
+    return 0;
+}
+*/
 #include <stdio.h>
 
 int main() {
